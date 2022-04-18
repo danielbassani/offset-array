2f1c5287728e965ed0e57c7a7ca7e89706a98ad3
dantheman0124 <daniel.bassani@outlook.com>
2022-04-18 16:18:20 -0400

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+function center(arr, num) {
+    let res = [];
+    if (typeof arr !== 'object' || arr === [] || arr[0] === undefined || typeof num != 'number') {
+        return res;
+    } else {
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === 'number') {
+                res.push(arr[i] - center);
+            }
+        }
+    }
+
+    return res;
+}
